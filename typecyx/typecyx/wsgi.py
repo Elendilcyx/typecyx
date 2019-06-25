16a778f6ee2ee95a33bb3e68c918f5af854d085e
cyx <425319694@qq.com>
2019-06-25 15:24:50 +0800

    add model

--- typecyx/typecyx/wsgi.py
+++ typecyx/typecyx/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-profile = os.environ.get('TYPECYX_PROFILE', 'develop')
+profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typecyx.settings.%s" % profile)
 
 application = get_wsgi_application()
